c35c1f731acd6b91f0db9290a10abdebdd0aa481
SaltApocalypse <super800800@icloud.com>
2025-10-29 01:40:46 +0800

    🎉 init(*):

---
+++ eslint.config.mjs
@@ -0,0 +1,64 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    extends: compat.extends(
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended"
+    ),
+
+    plugins: {
+      prettier,
+      "simple-import-sort": simpleImportSort,
+    },
+
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+        },
+      ],
+
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
